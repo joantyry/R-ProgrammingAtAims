a5be62fed8d7d1391ac15289b0d08f2a9c1216d2
joantyry <tyry7979@gmail.com>
2018-01-08 21:49:29 +0200

    Add files via upload

--- Intro_day1.r
+++ Intro_day1.r
@@ -31,7 +31,7 @@ options(digits = 5)#affects how numbers are displayed
 #ARITHMETIC
 2+5
 #seq(2,5) == 2:5
-(2:5)^c(2,3,1,0)
+(2:5)^c(2,3,1,0) 
 (2:5)^2
 (1:6)*(1:2)
 (1:5)-(0:1)
@@ -42,3 +42,35 @@ t.names <- c("Urs","Anna","Max","Pia")
 nchar(t.names)
 #string manipulations
 substring (t.names,3,4)
+
+#LOGICAL VECTORS
+#contain TRUE or FALSE
+rep(c(TRUE,FALSE), length = 6)
+(1:5)>=3
+#logical operators
+t.i <- (t.a >=2)&(t.a < 5)
+t.i
+t.a >= 2
+t.a < 5
+
+#SELECTING ELEMENTS
+getwd()
+setwd('/home/joan/Desktop/R-Programming')
+d.sport <- read.table('sport.dat')
+d.sport
+t.v[c(1,3,5)]
+d.sport[c(1,3,5),1:3] #1,3,5 rows and 1,2,3 columns.
+t.a[c(TRUE,FALSE,TRUE,TRUE,FALSE)]
+d.sport [c("OBRIEN","DVORAK"),c("shotp","javelin","score")]
+d.sport$shotp
+d.sport$shotp[c(5,1:3)]
+names(d.sport)
+colnames(d.sport)
+row.names(d.sport)
+d.sport[-(3:12),c("shotp","score")]# - means removing the rows
+d.sport[d.sport[,"shotp"]>16, c(2,7)]#setting conditions only data > 16 returned
+rm(t.v) #remove
+objects(d.sport)
+objects()
+str(d.sport)#structure
+ls()
